d21f7c0ec0960cc6a99df7b6bec6662201df7f4c
Dmitry.Sbrodov <dmitry.sbrodov@x5.ru>
2021-09-08 09:42:52 +0500

    Home task coroutines

--- app/src/main/java/ru/skillbranch/sbdelivery/screens/root/logic/RootFeature.kt
+++ app/src/main/java/ru/skillbranch/sbdelivery/screens/root/logic/RootFeature.kt
@@ -162,6 +162,4 @@ sealed class Command {
     //Android specific commands finish() startForResult, etc
 }
 
-interface IEffectHandler<E, M> {
-    suspend fun handle(effect: E, commit: (M) -> Unit)
-}
+
